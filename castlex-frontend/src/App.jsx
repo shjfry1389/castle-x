5e2f8c08b029e8bd0fe448260c3763cc0da6a771
Sharmin <sharmin@example.com>
2026-06-11 12:27:29 +0330

    Fix missing imports

--- castlex-frontend/src/App.jsx
+++ castlex-frontend/src/App.jsx
@@ -1,5 +1,14 @@
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import home from "./pages/home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Admin from "./pages/Admin";
+import Search from "./pages/Search";
+import Profile from "./pages/Profile";
+import Messages from "./pages/Messages";
+import Notifications from "./pages/Notifications";
+import Chat from "./pages/Chat";
 
 function Layout() {
   const location = useLocation();
