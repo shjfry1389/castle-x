ac23c94a7cbc8e6077126529dc6e7c51a2aaf9ca
Sharmin <sharmin@example.com>
2026-06-11 11:32:56 +0330

    Connect Render Backend

--- castlex-frontend/src/services/api.js
+++ castlex-frontend/src/services/api.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
-export default axios.create({
-  baseURL: "http://localhost:5000"
+const api = axios.create({
+  baseURL: "https://castle-x-backend.onrender.com",
 });
+
+export default api;
