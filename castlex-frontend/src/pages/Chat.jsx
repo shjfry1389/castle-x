5139554629ea68f374cc094dabb972b541f6d0c3
Sharmin <sharmin@example.com>
2026-06-15 15:54:04 +0330

    show date in chat messages

--- castlex-frontend/src/pages/Chat.jsx
+++ castlex-frontend/src/pages/Chat.jsx
@@ -206,9 +206,12 @@ return (
             {msg.created_at
               ? new Date(
                   msg.created_at
-                ).toLocaleTimeString([], {
-                  hour: "2-digit",
-                  minute: "2-digit",
+                      ).toLocaleString("fa-IR", {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
                 })
               : ""}
           </div>
